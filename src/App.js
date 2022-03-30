276295ea5917132f2dff8369a32b797b2eb63a6c
Muhammad Kashif <mkashif.aw@gmail.com>
2022-03-31 02:35:31 +0500

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
+          To check surge deploy
         </a>
       </header>
     </div>
